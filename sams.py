7d517560f9fb05f4aa6187282ab6fdcf73b832e3
Shemeen62 <fdoshemeen61@gmail.com>
2023-09-14 21:59:44 +0530

    latest changes for image processing

--- sams.py
+++ sams.py
@@ -1,108 +1,126 @@
 import sys
+
 import cv2
 import numpy as np
+import sheet
 import easyocr
+import utilities
 import xml.etree.cElementTree as ET
-import utils
 
-# Function to get an adaptive binary image
-def get_adaptive_binary_image(img):
+# Reading Image
+print('[ Step 1 ] Reading Image...')
+
+print('Number of arguments:', len(sys.argv), 'arguments.')
+print('Argument List:', str(sys.argv))
+
+filename = sys.argv[1]
+filename = filename.split(".")[0]
+sample = cv2.imread("./Samples/" + sys.argv[1])
+print('[ Step 1 ] Image Read Completed!')
+
+
+def adaptive_binary_image(img):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (5, 5), 0)
     cv2.imwrite("./Outputs/gray_" + filename + ".jpeg", gray)
     return cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 7, 4)
 
-# Function to get the biggest intensity contour
-def get_biggest_intensity_contour(contours):
+
+def rotated_sheet(img, img_adaptive_binary):
+    contours = external_contours(img_adaptive_binary)
+    biggest_contour = biggest_intensity_contour(contours)
+
+    img_raw_sheet = utilities.get_rotated_image_from_contour(img, biggest_contour)
+
+    img_raw_sheet = sheet.resize_to_right_ratio(img_raw_sheet)
+    img_binary_sheet_rotated = adaptive_binary_image(img_raw_sheet)
+
+    return img_raw_sheet, img_binary_sheet_rotated
+
+
+def biggest_intensity_contour(contours):
     sorted_contours = sorted(contours, key=cv2.contourArea, reverse=True)
     biggest_contour = sorted_contours[0]
     return biggest_contour
 
-# Function to get external contours
-def get_external_contours(img_1_channel):
+
+def external_contours(img_1_channel):
     contours, _ = cv2.findContours(img_1_channel, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     return contours
 
-# Function to process the image and extract text
-def process_image(filename):
-    # Reading Image
-    print('[ Step 1 ] Reading Image...')
-    sample = cv2.imread("./Samples/" + filename)
-    filename = filename.split(".")[0]
-    print('[ Step 1 ] Image Read Completed!')
 
-    # Getting Started
-    print("[ Step 2 ] Scaling Image...")
-    scale_percent = 40
-    width = int(sample.shape[1] * scale_percent / 100)
-    height = int(sample.shape[0] * scale_percent / 100)
-    dim = (width, height)
-    resized = cv2.resize(sample, dim, interpolation=cv2.INTER_AREA)
-    print("[ Step 2 ] Scaling Image Completed")
+# Starting the process
+print("[ Step 2 ] Image scaling ...")
+scale_percent = 40
+width = int(sample.shape[1] * scale_percent / 100)
+height = int(sample.shape[0] * scale_percent / 100)
+dim = (width, height)
 
-    print("[ Step 3 ] Binarizing, GreyScaling and Rotating Image...")
-    binary_image = get_adaptive_binary_image(resized)
-    utils.compareBoundingSizeimage_with_contours, img_binary_sheet_rotated = utils.rotated_sheet(resized, binary_image)
-    rotated_image = cv2.rotate(img_binary_sheet_rotated, cv2.ROTATE_90_CLOCKWISE)
-    shape = rotated_image.shape
-    new_height = int(shape[1] * 44 / 180)
-    sub_image = rotated_image[shape[0] - new_height: shape[0], 0: shape[1]]
-    shape = sub_image.shape
-    section_height = int(new_height / 6)
-    print("[ Step 3 ] Binarizing, GreyScaling and Rotating Image Completed!")
+resized = cv2.resize(sample, dim, interpolation=cv2.INTER_AREA)
+print("[ Step 2 ] Image Scaling Completed")
 
-    print("[ Step 4 ] Reading Text...")
-    reader = easyocr.Reader(['en'], gpu=False)
+#
+print("[ Step 3 ] Binarizing, GreyScaling and Rotating the Image...")
+binary_image = adaptive_binary_image(resized)
 
-    root = ET.Element("nsbm")
-    doc = ET.SubElement(root, "students")
+image_with_contours, img_binary_sheet_rotated = rotated_sheet(resized, binary_image)
 
-    init_y = 0
-    for i in range(6):
-        print("[ Step 4." + str(i + 1) + ".1 ] Reading Text of Row " + str(i + 1) + "...")
-        section = sub_image[0 + section_height * i: section_height * (i + 1), 0: shape[1]]
+rotated_image = cv2.rotate(img_binary_sheet_rotated, cv2.ROTATE_90_CLOCKWISE)
 
-        sig_section = section[0: section_height, shape[1] - int(shape[1] / 5): shape[1]]
-        sig_shape = sig_section.shape
-        h = int((sig_shape[0] * 8 / 10) / 2)
-        w = int((sig_shape[1] * 8 / 10) / 2)
-        x = int(sig_shape[1] / 2) - w
-        y = int(sig_shape[0] / 2) - h
-        cropped_sig = sig_section[y:y + (h * 2), x:x + (w * 2)]
+shape = rotated_image.shape
+new_height = int(shape[1] * 44 / 180)
+# 44:180
+sub_image = rotated_image[shape[0] - new_height: shape[0], 0: shape[1]]
 
-        section = sub_image[0 + section_height * i: section_height * (i + 1), int(shape[1] / 8): 4 * int(shape[1] / 5)]
+shape = sub_image.shape
+section_height = int(new_height / 6)
+print("[ Step 3 ] Completed Binarizing, GreyScaling and Rotating the Image!")
 
-        sig_percentage = ((float(np.sum(cropped_sig == 255)) / float(cropped_sig.size)) * 100)
-        result = reader.readtext(section, detail=0)
-        result[2] = result[2].replace("$", "S")
-        data = {
-            "index": result[0],
-            "name": result[2],
-            "attendance": sig_percentage > 5,
-        }
+print("[ Step 4 ] Reading Text...")
+reader = easyocr.Reader(['en'], gpu=False)
 
-        if len(result) > 3:
-            data['name'] = result[2] + " " + result[3]
-        student = ET.SubElement(doc, "student")
-        ET.SubElement(student, "index").text = result[0]
-        ET.SubElement(student, "attendance").text = "true" if sig_percentage > 5 else "false"
-        print("[ Step 4." + str(i + 1) + ".1 ] Reading Text of Row " + str(i + 1) + " Completed!")
+root = ET.Element("nsbm")
+doc = ET.SubElement(root, "students")
 
-        print("[ Step 4." + str(i + 1) + ".2 ] Saving Captured Signature of Row " + str(i + 1) + "...")
-        outputPath = "./Outputs/" + filename + "_" + result[0] + ".jpeg"
-        cv2.imwrite(outputPath, cropped_sig)
-        print("[ Step 4." + str(i + 1) + ".2 ] Saving Captured Signature of Row " + str(i + 1) + " Completed!")
+init_y = 0
+for i in range(6):
+    print("[ Step 4." + str(i + 1) + ".1 ] Reading Text of Row " + str(i + 1) + "...")
+    section = sub_image[0 + section_height * i: section_height * (i + 1), 0: shape[1]]
 
-    print("[ Step 4 ] Reading Text Completed!")
+    sig_section = section[0: section_height, shape[1] - int(shape[1] / 5): shape[1]]
+    sig_shape = sig_section.shape
+    h = int((sig_shape[0] * 8 / 10) / 2)
+    w = int((sig_shape[1] * 8 / 10) / 2)
+    x = int(sig_shape[1] / 2) - w
+    y = int(sig_shape[0] / 2) - h
+    cropped_sig = sig_section[y:y + (h * 2), x:x + (w * 2)]
 
-    print("[ Step 5 ] Saving XML...")
-    tree = ET.ElementTree(root)
-    tree.write("./Outputs/" + filename + "_info.xml")
-    print("[ Step 5 ] Saving XML Completed!")
+    section = sub_image[0 + section_height * i: section_height * (i + 1), int(shape[1] / 8): 4 * int(shape[1] / 5)]
 
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: python sams.py <image_filename>")
-    else:
-        filename = sys.argv[1]
-        process_image(filename)
+    sig_percentage = ((float(np.sum(cropped_sig == 255)) / float(cropped_sig.size)) * 100)
+    result = reader.readtext(section, detail=0)
+    result[2] = result[2].replace("$", "S")
+    data = {
+        "index": result[0],
+        "name": result[2],
+        "attendance": sig_percentage > 5,
+    }
+
+    if len(result) > 3:
+        data['name'] = result[2] + " " + result[3]
+    student = ET.SubElement(doc, "student")
+    ET.SubElement(student, "index").text = result[0]
+    ET.SubElement(student, "attendance").text = "true" if sig_percentage > 5 else "false"
+    print("[ Step 4." + str(i + 1) + ".1 ] Reading Text of Row " + str(i + 1) + " Completed!")
+
+    print("[ Step 4." + str(i + 1) + ".2 ] Saving Captured Signature of Row " + str(i + 1) + "...")
+    outputPath = "./Outputs/" + filename + "_" + result[0] + ".jpeg"
+    cv2.imwrite(outputPath, cropped_sig)
+    print("[ Step 4." + str(i + 1) + ".2 ] Saving Captured Signature of Row " + str(i + 1) + " Completed!")
+
+print("[ Step 4 ] Reading Text Completed!")
+
+print("[ Step 5 ] Saving to XML...")
+tree = ET.ElementTree(root)
+tree.write("./Outputs/" + filename + "_info.xml")
+print("[ Step 5 ] Saving XML Completed!")
--- sheet.py
+++ sheet.py
@@ -1,21 +1,20 @@
+from functools import cmp_to_key
 import cv2
 import numpy as np
-from functools import cmp_to_key
-import utils
+import utilities
+
 
 MIN_CELL_DIGIT_HEIGHT_RATIO = 2.5
 
-# Function to check if two lines should be merged
-def should_merge_lines(line_a, line_b, rho_distance, theta_distance):
+
+def __should_merge_lines(line_a, line_b, rho_distance, theta_distance):
     rho_a, theta_a = line_a[0].copy()
     rho_b, theta_b = line_b[0].copy()
-
-    if rho_b == rho_a and theta_b == theta_b:
+    if(rho_b == rho_a and theta_b == theta_b):
         return False
 
     theta_b = int(180 * theta_b / np.pi)
     theta_a = int(180 * theta_a / np.pi)
-
     if rho_b < 0:
         theta_b = theta_b - 180
 
@@ -28,48 +27,69 @@ def should_merge_lines(line_a, line_b, rho_distance, theta_distance):
     diff_theta = np.abs(theta_a - theta_b)
     rho_diff = np.abs(rho_a - rho_b)
 
-    if rho_diff < rho_distance and diff_theta < theta_distance:
+    if(rho_diff < rho_distance and diff_theta < theta_distance):
         return True
 
     return False
 
-# Function to merge two lines
+
+def resize_to_right_ratio(img, interpolation=cv2.INTER_LINEAR, width=750):
+    ratio_width = width / img.shape[1]
+    return cv2.resize(img, None, fx=ratio_width, fy=ratio_width, interpolation=interpolation)
+
+
 def merge_lines(line_a, line_b):
     rho_b, theta_b = line_b[0]
     rho_a, theta_a = line_a[0]
-
     if rho_b < 0:
         rho_b = np.abs(rho_b)
         theta_b = theta_b - np.pi
-
     if rho_a < 0:
         rho_a = np.abs(rho_a)
         theta_a = theta_a - np.pi
 
     average_theta = (theta_a + theta_b) / 2
     average_rho = (rho_a + rho_b) / 2
-
     if average_theta < 0:
         average_rho = -average_rho
         average_theta = np.abs(average_theta)
 
     return [[average_rho, average_theta]]
 
-# Function to merge nearby lines
+
+def get_merged_line(lines, line_a, rho_distance, degree_distance):
+    for i, line_b in enumerate(lines):
+        if line_b is False:
+            continue
+        if __should_merge_lines(line_a, line_b, rho_distance, degree_distance):
+            line_a = merge_lines(line_a, line_b)
+            lines[i] = False
+
+    return line_a
+
+
+def adaptive_binary_image(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (5, 5), 0)
+
+    return cv2.adaptiveThreshold(gray, 255,  cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 7, 4)
+
+
 def merge_nearby_lines(lines, rho_distance=30, degree_distance=20):
+
     lines = lines if lines is not None else []
     estimated_lines = []
-
     for line in lines:
         if line is False:
             continue
 
-        estimated_line = get_merged_line(lines, line, rho_distance, degree_distance)
+        estimated_line = get_merged_line(
+            lines, line, rho_distance, degree_distance)
         estimated_lines.append(estimated_line)
 
     return estimated_lines
 
-# Function to draw lines on an image
+
 def draw_lines(lines, img):
     if lines is not None:
         for line in lines:
@@ -80,9 +100,10 @@ def draw_lines(lines, img):
             y0 = b * rho
             pt1 = (int(x0 + 10000*(-b)), int(y0 + 10000*(a)))
             pt2 = (int(x0 - 10000*(-b)), int(y0 - 10000*(a)))
-            cv2.line(img, pt1, pt2, (255, 255, 255), 2)
+            cv2.line(img, pt1, pt2,
+                     (255, 255, 255), 2)
 
-# Function to sort bounding rectangles by upper left position
+
 def sort_by_upper_left_pos(rect_a, rect_b):
     x_a, y_a, _, _ = rect_a
     x_b, y_b, w_b, _ = rect_b
@@ -95,70 +116,123 @@ def sort_by_upper_left_pos(rect_a, rect_b):
         return y_a - y_b
     return (x_a - x_b_offset_positive)
 
-# Function to get the merged line
-def get_merged_line(lines, line_a, rho_distance, degree_distance):
-    for i, line_b in enumerate(lines):
-        if line_b is False:
-            continue
 
-        if should_merge_lines(line_a, line_b, rho_distance, degree_distance):
-            line_a = merge_lines(line_a, line_b)
-            lines[i] = False
+def get_rotated_sign_sheet_sheet(img, img_adaptive_binary):
+    contours = utilities.external_contours(img_adaptive_binary)
+    biggest_contour = utilities.biggest_intensity_contour(contours)
 
-    return line_a
+    img_raw_sign_sheet_sheet = utilities.get_rotated_image_from_contour(img, biggest_contour)
 
-# Function to filter bounding rectangles by dimensions
-def filter_by_dim(val, target_width, target_height):
+    img_raw_sign_sheet_sheet = resize_to_right_ratio(img_raw_sign_sheet_sheet)
+    img_binary_sheet_rotated = adaptive_binary_image(img_raw_sign_sheet_sheet)
+
+    return img_raw_sign_sheet_sheet, img_binary_sheet_rotated
+
+
+def generate_sign_sheet_sheet(img, num_rows_in_grid=19, max_num_cols=20):
+    img = resize_to_right_ratio(img)
+    img_adaptive_binary = adaptive_binary_image(img)
+
+    img_sign_sheet_sheet, img_binary_sign_sheet_sheet = get_rotated_sign_sheet_sheet(img, img_adaptive_binary)
+    img_binary_grid, img_binary_only_numbers = get_sign_sheet_grid(img_binary_sign_sheet_sheet)
+
+    sign_sheet_cells_bounding_rects, grid_bounding_rect = getting_the_sign_sheet_cells_bounding_rects(img_binary_grid, num_rows_in_grid, max_num_cols)
+
+    img_binary_only_numbers = utilities.get_bounding_rect_content(img_binary_only_numbers, grid_bounding_rect)
+    img_binary_sign_sheet_sheet = utilities.get_bounding_rect_content(img_binary_sign_sheet_sheet, grid_bounding_rect)
+    img_sign_sheet_sheet = utilities.get_bounding_rect_content(img_sign_sheet_sheet, grid_bounding_rect)
+
+    return img_sign_sheet_sheet, img_binary_sign_sheet_sheet, img_binary_only_numbers, sign_sheet_cells_bounding_rects
+
+
+def get_sign_sheet_grid(img_binary_sheet):
+    height, width = img_binary_sheet.shape
+    img_binary_sheet_morphed = img_binary_sheet.copy()
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+    img_binary_sheet_morphed = cv2.morphologyEx(img_binary_sheet_morphed, cv2.MORPH_DILATE, kernel)
+
+    sheet_binary_grid_horizontal = img_binary_sheet_morphed.copy()
+    sheet_binary_grid_vertical = img_binary_sheet_morphed.copy()
+    structuring_line_size = int(width / 5.0)
+    element = cv2.getStructuringElement(cv2.MORPH_RECT, (structuring_line_size, 1))
+    sheet_binary_grid_horizontal = cv2.morphologyEx(sheet_binary_grid_horizontal, cv2.MORPH_OPEN, element)
+    element = cv2.getStructuringElement(cv2.MORPH_RECT, (1, structuring_line_size))
+
+    sheet_binary_grid_vertical = cv2.morphologyEx(sheet_binary_grid_vertical, cv2.MORPH_OPEN, element)
+    img_binary_sheet_morphed = cv2.add(
+        sheet_binary_grid_vertical, sheet_binary_grid_horizontal)
+
+    rho_accumulator = 1
+    angle_accumulator = np.pi / 2
+    threshold_accumulator_votes = int(width/2)
+    grid_lines = cv2.HoughLines(img_binary_sheet_morphed, rho_accumulator,
+                                angle_accumulator, threshold_accumulator_votes)
+
+    img_binary_grid = np.zeros(
+        img_binary_sheet_morphed.shape, dtype=img_binary_sheet_morphed.dtype)
+    grid_lines = merge_nearby_lines(grid_lines)
+
+    draw_lines(grid_lines, img_binary_grid)
+    outer_border = np.array([
+        [1, height-1],
+        [1, 1],
+        [width-1, 1],
+        [width-1, height-1]
+    ])
+    cv2.drawContours(img_binary_grid, [outer_border], 0, (255, 255, 255), 3)
+    img_binary_sheet_only_digits = cv2.bitwise_and(img_binary_sheet, 255 - img_binary_sheet_morphed)
+
+    return img_binary_grid, img_binary_sheet_only_digits
+
+
+def __filter_by_dim(val, target_width, target_height):
     offset_width = target_width * 0.3
     offset_height = target_height * 0.3
     _, _, w, h = val
     return target_width - offset_width < w < target_width + offset_width and target_height - offset_height < h < target_height + offset_height
 
-# Function to find the most common area among bounding rectangles
-def get_most_common_area(bounding_rects, cell_resolution):
+
+def getting_the_most_common_area(bounding_rects, cell_resolution):
     cell_areas = [int(w*h/cell_resolution) for _, _, w, h in bounding_rects]
     counts = np.bincount(cell_areas)
     return bounding_rects[np.argmax(counts)]
 
-# Function to validate and find the sign sheet cell
+
 def validate_and_find_sign_sheet_cell(cells, bounding_rect):
     roi_x, roi_y, roi_w, roi_h = bounding_rect
     roi_center_x = roi_x + int(roi_w/2)
     roi_center_y = roi_y + int(roi_h/2)
-
     _, _, cell_width, cell_height = cells[0]
-
-    if not 2 < roi_w < cell_width or not (cell_height/MIN_CELL_DIGIT_HEIGHT_RATIO) < roi_h < cell_height:
+    if(not 2 < roi_w < cell_width or not (cell_height/MIN_CELL_DIGIT_HEIGHT_RATIO) < roi_h < cell_height):
         return None
 
     found_cell = None
-
     for rect in cells:
         x, y, w, h = rect
-        if x < roi_center_x < x + w and y < roi_center_y < y + h:
+        if(x < roi_center_x < x + w and y < roi_center_y < y + h):
             found_cell = rect
             break
 
     return found_cell
 
-# Function to get sign sheet cells bounding rectangles
-def get_sign_sheet_cells_bounding_rects(img_binary_grid, num_rows_in_grid=19, max_num_cols=20):
+
+def getting_the_sign_sheet_cells_bounding_rects(img_binary_grid, num_rows_in_grid=19, max_num_cols=20):
     binary_grid_contours, _ = cv2.findContours(img_binary_grid, cv2.RETR_LIST,
                                                cv2.CHAIN_APPROX_SIMPLE)
 
     sheet_width = img_binary_grid.shape[1]
 
     cell_min_width = (sheet_width/max_num_cols)
-    sign_sheet_cells_bounding_rects = [cv2.boundingRect(cnt) for cnt in binary_grid_contours if utils.wider_than(cnt, cell_min_width)]
+    sign_sheet_cells_bounding_rects = [cv2.boundingRect(cnt) for cnt in binary_grid_contours if utilities.wider_than(cnt, cell_min_width)]
 
     cell_resolution = (sheet_width/50) ** 2
 
-    _, _, target_width, target_height = get_most_common_area(sign_sheet_cells_bounding_rects, cell_resolution)
+    _, _, target_width, target_height = getting_the_most_common_area(sign_sheet_cells_bounding_rects, cell_resolution)
 
     if len(sign_sheet_cells_bounding_rects) < num_rows_in_grid:
-        print("ERROR: No grid cells found.")
+        print("ERROR: Grid cells not found.")
 
-    sign_sheet_cells_bounding_rects = list(filter(lambda x: filter_by_dim(x, target_width, target_height), sign_sheet_cells_bounding_rects))
+    sign_sheet_cells_bounding_rects = list(filter(lambda x: __filter_by_dim(x, target_width, target_height), sign_sheet_cells_bounding_rects))
 
     num_cells = len(sign_sheet_cells_bounding_rects)
     correct_num_cells_in_grid = (num_cells >= num_rows_in_grid and num_cells % num_rows_in_grid == 0)
@@ -166,10 +240,10 @@ def get_sign_sheet_cells_bounding_rects(img_binary_grid, num_rows_in_grid=19, ma
     if not correct_num_cells_in_grid:
         print("ERROR: num found:", num_cells)
 
-    sign_sheet_grid_bounding_rect = utils.concatenate_bounding_rects(sign_sheet_cells_bounding_rects)
+    sign_sheet_grid_bounding_rect = utilities.concatenate_bounding_rects(sign_sheet_cells_bounding_rects)
 
     shift_x, shift_y, _, _ = sign_sheet_grid_bounding_rect
-    sign_sheet_cells_bounding_rects = list(map(lambda x: utils.move_bounding_rect(x, -shift_x, -shift_y), sign_sheet_cells_bounding_rects))
-    sign_sheet_cells_bounding_rects = sorted(sign_sheet_cells_bounding_rects, key=cmp_to_key(sort_by_upper_left_pos))
-    
+    sign_sheet_cells_bounding_rects = list(map(lambda x: utilities.move_bounding_rect(x, -shift_x, -shift_y), sign_sheet_cells_bounding_rects))
+    sign_sheet_cells_bounding_rects = sorted(
+        sign_sheet_cells_bounding_rects, key=cmp_to_key(sort_by_upper_left_pos))
     return sign_sheet_cells_bounding_rects, sign_sheet_grid_bounding_rect
